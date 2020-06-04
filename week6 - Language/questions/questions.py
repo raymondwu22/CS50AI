bb40efc319c03e694c997b1c14a66fe8f985dc2a
Ray Wu <raywupharmd@outlook.com>
2020-06-03 20:32:00 -0400

    completed week 6b pset questions - Write an AI to answer questions.

--- week6 - Language/questions/questions.py
+++ week6 - Language/questions/questions.py
@@ -1,5 +1,8 @@
 import nltk
 import sys
+import os
+import math
+import string
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -13,6 +16,7 @@ def main():
 
     # Calculate IDF values across files
     files = load_files(sys.argv[1])
+
     file_words = {
         filename: tokenize(files[filename])
         for filename in files
@@ -48,7 +52,14 @@ def load_files(directory):
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    files = dict()
+    for filename in os.listdir(directory):
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            files[f] = contents
+
+    return files
 
 
 def tokenize(document):
@@ -59,7 +70,17 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    # Extract words
+    document = document.lower().translate(str.maketrans('', '', string.punctuation))
+    contents = [
+        # All words in the returned list should be lowercased.
+        word.lower() for word in
+        nltk.word_tokenize(document)
+        # Filter out punctuation and stopwords
+        if word not in nltk.corpus.stopwords.words("english")
+    ]
+    return contents
 
 
 def compute_idfs(documents):
@@ -70,8 +91,20 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    idfs = dict()
+    # create set of words
+    words = set()
+    for filename in documents:
+        for word in documents[filename]:
+            words.add(word)
 
+    for word in words:
+        f = sum(word in documents[filename] for filename in documents)
+        idf = math.log(len(documents) / f)
+        idfs[word] = idf
+
+    return idfs
 
 def top_files(query, files, idfs, n):
     """
@@ -80,7 +113,18 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    tfidfs = dict()
+    for filename in files:
+        tfidfs[filename] = 0
+        for word in query:
+            tf = files[filename].count(word)
+            tfidfs[filename] += tf * idfs[word]
+
+    # Sort and get top n TF-IDFs for each file
+    result = sorted(tfidfs.keys(), key=lambda x: tfidfs[x], reverse=True)
+    result = list(result)
+    return result[:n]
 
 
 def top_sentences(query, sentences, idfs, n):
@@ -91,7 +135,25 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    sentence_tfidfs = dict()
+    for sentence in sentences:
+        words = sentences[sentence]
+        total_idf = 0
+        count = 0
+        for word in query:
+            if word in words:
+                # Sentences should be ranked according to “matching word measure”: namely,
+                # the sum of IDF values for any word in the query that also appears in the sentence.
+                total_idf += idfs[word]
+                count += 1
+        # Query term density is defined as the proportion of words in the sentence that are also words in the query.
+        sentence_tfidfs[sentence] = (total_idf, count/len(words))
+
+    # Sort and get top n TF-IDFs for each file
+    result = sorted(sentence_tfidfs.keys(), key=lambda x: sentence_tfidfs[x], reverse=True)
+    result = list(result)
+    return result[:n]
 
 
 if __name__ == "__main__":
